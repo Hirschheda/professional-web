417c1cef0e4614720b5fad82d867a3b23d69f1d7
Hirschheda <hirschchheda@gmail.com>
2024-05-26 23:58:44 -0700

    my second commit

--- src/App.js
+++ src/App.js
@@ -46,7 +46,6 @@ function App() {
         <Extracurriculars />
         <Contact />
       </main>
-      <Footer />
       <a href="#" className="back-to-top d-flex align-items-center justify-content-center"><i className="bi bi-arrow-up-short"></i></a>
     </div>
   );
